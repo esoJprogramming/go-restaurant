6dd255efc236e966360205b23e4856275d120bc9
Jose Oc <40551163+esoJprogramming@users.noreply.github.com>
2020-05-29 22:31:21 -0300

    end

--- src/pages/Dashboard/index.tsx
+++ src/pages/Dashboard/index.tsx
@@ -27,7 +27,9 @@ const Dashboard: React.FC = () => {
 
   useEffect(() => {
     async function loadFoods(): Promise<void> {
-      // TODO LOAD FOODS
+      const response = await api.get('/foods');
+
+      setFoods(response.data);
     }
 
     loadFoods();
@@ -37,7 +39,9 @@ const Dashboard: React.FC = () => {
     food: Omit<IFoodPlate, 'id' | 'available'>,
   ): Promise<void> {
     try {
-      // TODO ADD A NEW FOOD PLATE TO THE API
+      const response = await api.post('/foods', { ...food, available: true });
+
+      setFoods([...foods, response.data]);
     } catch (err) {
       console.log(err);
     }
@@ -46,11 +50,28 @@ const Dashboard: React.FC = () => {
   async function handleUpdateFood(
     food: Omit<IFoodPlate, 'id' | 'available'>,
   ): Promise<void> {
-    // TODO UPDATE A FOOD PLATE ON THE API
+    const foodiesCopied = [...foods];
+    const updatedFoodIndex = foods.findIndex(f => f.id === editingFood.id);
+
+    foodiesCopied[updatedFoodIndex].description = food.description;
+    foodiesCopied[updatedFoodIndex].price = food.price;
+    foodiesCopied[updatedFoodIndex].name = food.name;
+    foodiesCopied[updatedFoodIndex].image = food.image;
+
+    setFoods(foodiesCopied);
+
+    await api.put(
+      `/foods/${foodiesCopied[updatedFoodIndex].id}`,
+      foodiesCopied[updatedFoodIndex],
+    );
   }
 
   async function handleDeleteFood(id: number): Promise<void> {
-    // TODO DELETE A FOOD PLATE FROM THE API
+    await api.delete(`foods/${id}`);
+
+    const foodsWithOutFoodDeleted = foods.filter(food => food.id !== id);
+
+    setFoods(foodsWithOutFoodDeleted);
   }
 
   function toggleModal(): void {
@@ -62,7 +83,9 @@ const Dashboard: React.FC = () => {
   }
 
   function handleEditFood(food: IFoodPlate): void {
-    // TODO SET THE CURRENT EDITING FOOD ID IN THE STATE
+    setEditingFood(food);
+
+    toggleEditModal();
   }
 
   return (
